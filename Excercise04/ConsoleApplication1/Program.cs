597b9e317425833173a06adbe9ae45e22aac33e4
tranminhhi2000 <tranminhhi2732000@gmail.com>
2019-10-01 15:27:06 +0700

    yareyare

---
+++ Excercise04/ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            InputInt(array);
+        }
+        public static void InputInt(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.Write("Phan tu - {0} : ", 1);
+                Arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static bool Findnum5(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+
+                if (Arr[i] == 5)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool Findnum5or6(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+
+                if (Arr[i] == 5)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (Arr[i] == 6)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static bool Findnum5and6(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+
+                if (Arr[i] == 5 )
+                {
+                    for (int j = 0; j < Arr.Length; j++)
+                    {
+                        if (Arr[j] == 6)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Excercise04/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFindnum5()
+        {
+            int [] ar1 = new int[5]{1,3,4,5,6};
+            bool result3 = Program.Findnum5(ar1);
+            Assert.AreEqual(true, result3);
+
+            int[] ar2 = new int[5] { 1, 3, 4, 7, 6 };
+            bool result4 = Program.Findnum5(ar2);
+            Assert.AreEqual(false, result4);
+        }
+        [TestMethod]
+        public void TestFindnum5or6()
+        {
+            int[] ar1 = new int[5] { 1, 3, 4, 5, 6 };
+            bool result3 = Program.Findnum5or6(ar1);
+            Assert.AreEqual(true, result3);
+
+            int[] ar2 = new int[5] { 1, 3, 4, 7, 6 };
+            bool result4 = Program.Findnum5or6(ar2);
+            Assert.AreEqual(true, result4);
+
+            int[] ar3 = new int[5] { 1, 3, 4, 7, 5 };
+            bool result5 = Program.Findnum5or6(ar3);
+            Assert.AreEqual(true, result4);
+
+            int[] ar4 = new int[5] { 1, 3, 4, 7, 4 };
+            bool result6 = Program.Findnum5or6(ar4);
+            Assert.AreEqual(false, result6);
+        }
+        [TestMethod]
+        public void TestFindnum5and6()
+        {
+            int[] ar1 = new int[5] { 1, 3, 4, 5, 6 };
+            bool result3 = Program.Findnum5and6(ar1);
+            Assert.AreEqual(true, result3);
+
+            int[] ar2 = new int[5] { 1, 3, 4, 7, 6 };
+            bool result4 = Program.Findnum5and6(ar2);
+            Assert.AreEqual(false, result4);
+
+            int[] ar3 = new int[5] { 1, 3, 4, 7, 5 };
+            bool result5 = Program.Findnum5and6(ar3);
+            Assert.AreEqual(false, result4);
+
+            int[] ar4 = new int[5] { 1, 3, 4, 7, 4 };
+            bool result6 = Program.Findnum5and6(ar4);
+            Assert.AreEqual(false, result6);
+        }
+    }
+}
